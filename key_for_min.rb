d934ff2258f92836a33e4dd02b5bd11f8faf574e
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-08-07 18:27:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   else
     min_value=nil
     min_key=nil
-    name_hash.each do |key,value| 
+    name_hash.each do |key,value|
     if min_value==nil||value<min_value
       min_value=value
       min_key=key
